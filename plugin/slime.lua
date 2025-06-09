786767757a5b9067f919fdff9383640c1e4a6d07
matt-888 <matt.mccabe.888@icloud.com>
2025-06-09 10:16:24 +1000

    use tmux keyword target

--- plugin/slime.lua
+++ plugin/slime.lua
@@ -2,7 +2,7 @@ MiniDeps.add("jpalardy/vim-slime")
 
 vim.g.slime_target = "tmux"
 vim.g.slime_bracketed_paste = 1
-vim.g.slime_default_config = { socket_name = "default", target_pane = ".1" }
+vim.g.slime_default_config = { socket_name = "default", target_pane = "{top-left}" }
 vim.g.slime_dont_ask_default = 1
 vim.g.slime_python_ipython = 1
 vim.g.slime_no_mappings = 1
